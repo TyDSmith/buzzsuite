8316cf4e847fbf9039414609c011d9be46cfb4b0
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-06-19 23:09:15 -0700

    login form styling update

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import './login.css';
 
-function Login (props) {
+function Login(props) {
     return (
         <div>
-            <from>
-                <input type="text" name="username" placeholder="Username"/>
-                <input type="password" name="password" />
-                <button type="button" >Submit</button>
-            </from>
-            <a href="/signup"><button>Sign up</button></a>
+            <div class="login-modal">
+                <img src="../images/logos/BuzzSuite.png" width="40%;" />
+                <form class="login-form">
+                    <input type="text" name="username" placeholder="Username" class="login-form-input" />
+                    <br />
+                    <input type="password" name="password" placeholder="Password" class="login-form-input" />
+                    <br />
+                    <button type="button" class="login-form-button">Submit</button>
+                </form>
+                <a href="/signup"><button>Sign up</button></a>
+
+
+            </div>
+
         </div>
+
+
     )
 }
 
