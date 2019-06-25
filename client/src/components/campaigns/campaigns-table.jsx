7dbde96508371b5077a7a8e20301ade9614efaa8
coachO88 <rangel52088@gmail.com>
2019-06-24 21:06:57 -0700

    Rendering

--- client/src/components/campaigns/campaigns-table.jsx
+++ client/src/components/campaigns/campaigns-table.jsx
@@ -2,15 +2,32 @@ import React, { Component } from "react";
 // Import React Table
 import ReactTable from "react-table";
 import "react-table/react-table.css";
-import { makeData } from "../../Utils";
+import API from "../../Utils";
 
 class CampaignsTable extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: makeData()
-    };
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     data: makeData()
+  //   };
+  // }
+
+  state = {
+    influencers: []
+  };
+
+  componentDidMount() {
+    this.loadInfluencers();
   }
+
+  loadInfluencers = () => {
+    API.getInfluencers()
+      // .then(res => this.setState({ influencers: res.data }))
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+      console.log(this.data)
+  };
+
   render() {
     const { data } = this.state;
     return (
