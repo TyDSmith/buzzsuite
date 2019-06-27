c75e6a66c6e39842848669f922ac0c84589adfa0
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-06-27 14:25:57 -0700

    update for merge conflict

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -26,32 +26,3 @@ class NavBar extends Component {
 
 export default NavBar;
 
-
-        // <nav className="navbar navbar-default">
-        //     <div className="container">
-        //         <div className="navbar-header">
-        //             <a class="navbar-brand" href="/">
-        //                 <img alt="Brand" src="../images/logos/BuzzSuite.png" height="30" />
-        //             </a>
-
-        //             <ul className="nav navbar-nav">
-        //                 {/* <div>Header Logo</div> */}
-        //                 <li>
-        //                     <Link to={"/"}>Home </Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to={"/dashboard"}>Dashboard </Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to={"/influencers"}>Influencers </Link>
-        //                 </li>
-        //                 <li>
-        //                     <Link to={"/campaigns"}>Campaigns </Link>
-        //                 </li>
-        //                 <li class="nav-login">
-        //                     <Link to={"/login"}>Login </Link>
-        //                 </li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </nav>
