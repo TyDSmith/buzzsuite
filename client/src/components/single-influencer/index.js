d0811826bc5f2906b184338381ca6a3de9739d95
coachO88 <rangel52088@gmail.com>
2019-06-26 21:31:02 -0700

    rendered some data

--- client/src/components/single-influencer/index.js
+++ client/src/components/single-influencer/index.js
@@ -3,16 +3,24 @@ import InfluencerProfile from './influencer-profile';
 import CampaignInputForm from './campaign-input-form';
 import InfluencerHistory from './influencer-history';
 import '../single-influencer/single-influencer.css';
-
+import API from '../../Utils'
 
 function SingleInfluencer(props) {
+
+function getHistory() {
+    API.getInfluencers().then((data) => console.log(data))
+};
+
+
+
+
     return (
         <div>
             <h2>Influencer name goes here</h2>
             <h1>Welcome Back, {props.UserInfo.data.firstName} {props.UserInfo.data.lastName} </h1>
             <div class="single-influencer-div">
                 <div class="column column-left">
-                    <InfluencerProfile />
+                    <InfluencerProfile UserInfo={props.UserInfo}/>
                 </div>
                 <div class="column column-center">
                     <InfluencerHistory />
