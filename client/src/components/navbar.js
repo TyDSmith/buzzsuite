0055d2587973b337750e232340d2e15241e1423d
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-07-01 20:04:43 -0700

    update readme and navbar styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,31 +4,31 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import App from "../App";
 
 class NavBar extends Component {
-    
+
     render() {
-    let UserLoggedIn = this.props.UserLoggedIn;
-    let loginButton;
-    if (UserLoggedIn) {
-      loginButton = <Link onClick={this.props.signout}> signout </Link>
-    } else {
-      loginButton = <Link to="/login"> Login </Link>
-    }
-      return (
-        <div>
-          <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/home">
-              <img src="../images/logos/BuzzSuite.png" alt="logo" height="25" />
-            </Navbar.Brand>
-            <Nav className="mr-auto">
-              <Link to="/campaigns"> Campaigns </Link>
-              <Link to="/Influencers"> Influencers </Link>
-              <Link to="/dashboard"> Dashboard </Link>
-            </Nav>
-            <Form inline>
-              {loginButton}
-            </Form>
-          </Navbar>
-        </div>
+        let UserLoggedIn = this.props.UserLoggedIn;
+        let loginButton;
+        if (UserLoggedIn) {
+            loginButton = <Link onClick={this.props.signout}> signout </Link>
+        } else {
+            loginButton = <Link to="/login"> Login </Link>
+        }
+        return (
+            <div>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar.Brand href="/home">
+                        <img src="../images/logos/BuzzSuite.png" alt="logo" height="25" />
+                    </Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Link to="/campaigns" className="navlinks"> Campaigns </Link>
+                        <Link to="/Influencers" className="navlinks"> Influencers </Link>
+                        <Link to="/dashboard" className="navlinks" > Dashboard </Link>
+                    </Nav>
+                    <Form inline>
+                        {loginButton}
+                    </Form>
+                </Navbar>
+            </div>
         );
     }
 }
