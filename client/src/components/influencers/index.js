1995eeec26538b31fa15ba7102ce4b665a4b257c
coachO88 <rangel52088@gmail.com>
2019-07-05 11:59:14 -0700

    Updated

--- client/src/components/influencers/index.js
+++ client/src/components/influencers/index.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-import "react-table/react-table.css";
-=======
 import React, { Component } from 'react';
->>>>>>> 16c65f8d32ada2010370b788477d7148c8ac3342
 import InfluencersTable from './influencers-table';
 import InfluencersFilter from './influencers-filter';
 import AddInfluencerButton from './add-influencer-btn';
 
 class Influencers extends Component {
 
-<<<<<<< HEAD
-    render(){
-    return (
-        <div>
-            <div class="page-heading">
-                <h1 class="page-title">Influencers</h1>
-                <span class="add-btn-span"><AddInfluencerButton /></span>
-            </div>
-            <div id="influencersfilterdiv">
-                <InfluencersFilter />
-=======
     render() {
         return (
             <div>
@@ -33,15 +17,10 @@ class Influencers extends Component {
                     <InfluencersFilter />
                 </div>
                 <InfluencersTable />
->>>>>>> 16c65f8d32ada2010370b788477d7148c8ac3342
             </div>
         )   
     }
 }
-<<<<<<< HEAD
-};
-=======
->>>>>>> 16c65f8d32ada2010370b788477d7148c8ac3342
 
 export default Influencers;
 
