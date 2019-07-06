b00e11aaf356b35ca4bd45f20d9a5fae0534c51f
coachO88 <rangel52088@gmail.com>
2019-07-06 13:31:45 -0700

    Updated Add Campaign and Add Influencer Forms

--- client/src/components/single-influencer/single-campaign-input-form.jsx
+++ client/src/components/single-influencer/single-campaign-input-form.jsx
@@ -76,11 +76,6 @@ class SingleCampaignInputForm extends Component {
           </label>
           <br/>
           <label>
-            Clicks:
-            <input onChange = {this.onChange.bind(this)} type="number" name="clicks" />
-          </label>
-          <br/>
-          <label>
             Channel:
             <input onChange = {this.onChange.bind(this)} type="text" name="channel" />
           </label>
