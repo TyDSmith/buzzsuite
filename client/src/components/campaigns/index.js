21c00b22e37fb42a90451cb17be56906bb614d1a
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-07-08 15:32:43 -0700

    various styling updates

--- client/src/components/campaigns/index.js
+++ client/src/components/campaigns/index.js
@@ -1,4 +1,4 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import CampaignsTable from './campaigns-table';
 import AddCampaignButton from './add-campaign-btn';
 
@@ -9,40 +9,40 @@ import API from "../utility/API"
 
 
 class Campaigns extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          campaigns: []
-        };
-      }
-      
-      componentDidMount() {
-        this.loadCampaign();
-      }
-      
-      loadCampaign = () => {
-        API.getCampaignById(this.props.UserInfo.data._id)
-          .then((res) => {
-            this.setState({ campaigns: res.data })
-          
-          })
-          .catch(err => console.log(err));
-          
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaigns: []
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <div class="page-heading">
-                    <h1 class="page-title">Campaigns</h1>
-                    <span class="add-btn-span"><AddCampaignButton UserInfo={this.props.UserInfo} loadCampaign={this.loadCampaign}/></span>
-                </div>
-                <CampaignsTable campaigns={this.state.campaigns} loadCampaign={this.loadCampaign}/>
-           <Footer />
-            </div>
-        )
-    }
-    
+  componentDidMount() {
+    this.loadCampaign();
+  }
+
+  loadCampaign = () => {
+    API.getCampaignById(this.props.UserInfo.data._id)
+      .then((res) => {
+        this.setState({ campaigns: res.data })
+
+      })
+      .catch(err => console.log(err));
+
+  };
+
+  render() {
+    return (
+      <div className="page-start">
+        <div class="page-heading">
+          <h1 class="page-title">Campaigns</h1>
+          <span class="add-btn-span"><AddCampaignButton UserInfo={this.props.UserInfo} loadCampaign={this.loadCampaign} /></span>
+        </div>
+        <CampaignsTable campaigns={this.state.campaigns} loadCampaign={this.loadCampaign} />
+        <Footer />
+      </div >
+    )
+  }
+
 }
 
 export default Campaigns;
