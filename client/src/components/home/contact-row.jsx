b614633de278f1415271c47026f6ca1197885128
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-07-08 00:16:11 -0700

    footer and contact div

---
+++ client/src/components/home/contact-row.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class ContactRow extends Component {
+  render() {
+    return (
+      <div>
+        <div className="home-contact-div">
+          <div className="home-contact-content">
+            <h4>Contact us</h4>
+            <button className="contact-button-home">Learn More</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContactRow;
